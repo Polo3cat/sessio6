5187a4d283829ab8bac3bee0a76bf2e245517b7a
Pablo <polo3ies@gmail.com>
2015-04-08 16:46:33 +0200

    Modificat el main 	modified:   ex_611.cc

--- ex_611.cc
+++ ex_611.cc
@@ -2,5 +2,9 @@
 
 int main()
 {
-  
+  cout << "La marca es el 0, pots començar a escriure les parelles:" << endl;
+  stack<parint> stackpi;
+  llegir_stack_parint(stackpi, 0);
+  cout << "El stack es aixi:" << endl;
+  escriure_stack_parint(stackpi);
 }
