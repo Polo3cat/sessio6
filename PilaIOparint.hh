28e6cb6d8eb74064cfee81ee130bee444a66cbae
Pablo <polo3ies@gmail.com>
2015-04-08 17:44:01 +0200

    Afegida funcio de sumar k a segon parell 	modified:   PilaIOparint.hh

--- PilaIOparint.hh
+++ PilaIOparint.hh
@@ -36,6 +36,8 @@ void escriure_stack_parint (stack<parint> s)
 }
 
 void busca_parella (stack<parint> s, int candidat)
+//PRE: true
+//POST: s'escriu al canal estandard de sortida el candidat i la seva parella, si no en té no es fa res
 {
   bool found = false;
   parint aux;
@@ -51,6 +53,25 @@ void busca_parella (stack<parint> s, int candidat)
     }
     s.pop();
   }
+}
+
+stack<parint> afegeix_k_sobre_copia (stack<parint>& a, int k)
+//PRE: a no es un stack buit
+//POST: retorna un stack igual al primer més k a la segona parella dels parint
+{
+  stack<parint> s = a;
+  stack<parint> aux;
+  parint paraux;
+  while (not s.empty()) {
+    paraux = s.top;
+    paraux.segon += k;
+    aux.push(paraux);
+  }
+  while (not aux.empty()) {
+    paraux = aux.top();
+    s.push(paraux);
+  }
+  return s;
 }
 
 #endif
